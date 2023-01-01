2cbc6a60c671cd067bb0485d568988095c9d1ace
Hümay <456789hz@gmail.com>
2023-01-01 16:19:52 +0300

    add tests

--- src/test/java/edu/ieu/tr/cvm/DatabaseTest.java
+++ src/test/java/edu/ieu/tr/cvm/DatabaseTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class DatabaseTest {
 
     @Test
-    public void testGetInstance() throws SQLException, ClassNotFoundException { //test
+    public void testGetInstance() throws SQLException, ClassNotFoundException {
         assertNull(Database.getInstance());
     }
 
@@ -20,6 +20,7 @@ public class DatabaseTest {
     @Test
     public void testInsert() throws SQLException, ClassNotFoundException {
         int oldlength=Database.getInstance().getAll().size();
+        Database.getInstance().open();
         Database.getInstance().insert(new Cv("hümay",2001,3,"humayzehraozer@gmail.com","student","home","address","05533000958","github.com",new HashMap<>(),new HashMap<>(),new HashMap<>(),new ArrayList<>()));
         int newlength=Database.getInstance().getAll().size();
         assertEquals(newlength, oldlength + 1);
--- src/test/java/edu/ieu/tr/cvm/controllers/AppControllerTest.java
+++
@@ -1,8 +0,0 @@
-package edu.ieu.tr.cvm.controllers;
-
-import junit.framework.TestCase;
-
-public class AppControllerTest extends TestCase {
-
-
-}
