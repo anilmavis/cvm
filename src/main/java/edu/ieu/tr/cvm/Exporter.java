066d7bd35fb06a7d6d28f64d9a38ac828059faef
anilmavis <77068958+anilmavis@users.noreply.github.com>
2023-01-03 18:28:27 +0300

    Update Exporter.java

--- src/main/java/edu/ieu/tr/cvm/Exporter.java
+++ src/main/java/edu/ieu/tr/cvm/Exporter.java
@@ -12,18 +12,104 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import javafx.stage.FileChooser;
 
 public final class Exporter {
-    public static void export(Cv cv) throws IOException {
+    public static void export(Cv c) throws IOException {
         PDDocument document = new PDDocument();
         PDPage page = new PDPage();
         document.addPage(page);
         PDPageContentStream stream = new PDPageContentStream(document, page);
-        stream.setFont(PDType1Font.TIMES_ROMAN, 12);
+        int padding = 24;
+        int width = (int) page.getMediaBox().getWidth();
+        int height = (int) page.getMediaBox().getHeight();
+        stream.setFont(PDType1Font.TIMES_ROMAN, padding);
+        stream.beginText();
+        if (c instanceof AcademicCv cv) {
+            stream.newLineAtOffset(width / 4, height - padding);
+            stream.showText("Full name: " + cv.getFullName());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Birth year: " + cv.getBirthYear());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("GPA: " + cv.getGpa());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Home address: " + cv.getHomeAddress());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Job address: " + cv.getJobAddress());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Phone: " + cv.getPhone());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Website: " + cv.getWebsite());
+            cv.getSkills().forEach((k, v) -> {
+                    try {
+                        stream.newLine();
+						stream.newLineAtOffset(0, -padding);
+                        stream.showText(k + ", proficiency: " + v + " / 10");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+                });
+            cv.getEducation().forEach((k, v) -> {
+                    try {
+                        stream.newLine();
+						stream.newLineAtOffset(0, -padding);
+                        stream.showText(k + ", register year: " + v);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+                });
+        } else {
+            Cv cv = c;
+            System.out.println(width + " " + height);
+            stream.newLineAtOffset(width / 4, height - padding);
+            stream.showText("Full name: " + cv.getFullName());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Birth year: " + cv.getBirthYear());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("GPA: " + cv.getGpa());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Home address: " + cv.getHomeAddress());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Phone: " + cv.getPhone());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("Website: " + cv.getWebsite());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("SKILLS:");
+            cv.getSkills().forEach((k, v) -> {
+                    try {
+                        stream.newLine();
+						stream.newLineAtOffset(0, -padding);
+                        stream.showText(k + ", proficiency: " + v + " / 10");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+                });
+            cv.getEducation().forEach((k, v) -> {
+                    try {
+                        stream.newLine();
+						stream.newLineAtOffset(0, -padding);
+                        stream.showText(k + ", register year: " + v);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+                });
+        }
+        stream.endText();
         stream.close();
         FileChooser chooser = new FileChooser();
-        chooser.setInitialFileName(cv.getFullName() + new SecureRandom().nextInt() + ".pdf");
+        chooser.setInitialFileName(c.getFullName() + c.getBirthYear() + ".pdf");
         chooser.setTitle("export");
         File file = chooser.showSaveDialog(null);
-        
+
         if (file != null) {
             document.save(file);
         }
