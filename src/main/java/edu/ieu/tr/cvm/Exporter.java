f463bfe67f2c5de025068d5e53156e60faf8cb6f
anilmavis <77068958+anilmavis@users.noreply.github.com>
2023-01-04 14:08:54 +0300

    Update Exporter.java

--- src/main/java/edu/ieu/tr/cvm/Exporter.java
+++ src/main/java/edu/ieu/tr/cvm/Exporter.java
@@ -9,6 +9,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.state.RenderingMode;
 
 import javafx.stage.FileChooser;
 
@@ -23,118 +24,179 @@ public final class Exporter {
         int height = (int) page.getMediaBox().getHeight();
         stream.setFont(PDType1Font.TIMES_ROMAN, padding);
         stream.beginText();
-        
+
         if (c instanceof AcademicCv cv) {
             stream.newLineAtOffset(width / 8, height - padding);
-            
-            stream.showText("• Full name: " + cv.getFullName());
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Full name: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getFullName());
             stream.newLine();
             stream.newLineAtOffset(0, -padding);
-            stream.showText("• Birth year: " + cv.getBirthYear());
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Birth year: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText("" + cv.getBirthYear());
             stream.newLine();
             stream.newLineAtOffset(0, -padding);
-            stream.showText("• GPA: " + cv.getGpa());
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• GPA: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText("" + cv.getGpa());
+            stream.newLine();
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("• Email: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getEmail());
+            stream.newLine();
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.newLineAtOffset(0, -padding);
+            stream.showText("• Description: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getDescription());
+            stream.newLine();
+
+            stream.newLineAtOffset(0, -padding);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Home address: ");
+            stream.showText(cv.getHomeAddress());
             stream.newLine();
             stream.newLineAtOffset(0, -padding);
-            stream.showText("• Email: " + cv.getEmail());
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Job address: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getJobAddress());
+            stream.newLine();
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.newLineAtOffset(0, -padding);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Phone: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getPhone());
             stream.newLine();
             stream.newLineAtOffset(0, -padding);
-            stream.showText("• Description: " + cv.getDescription());
-            stream.newLine();
-            
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Home address: " + cv.getHomeAddress());
-            stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Job address: " + cv.getJobAddress());
-            stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Phone: " + cv.getPhone());
-            stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Website: " + cv.getWebsite());
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Website: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getWebsite());
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
             stream.showText("• SKILLS:");
+            stream.setRenderingMode(RenderingMode.FILL);
             cv.getSkills().forEach((k, v) -> {
-                    try {
-                        stream.newLine();
-						stream.newLineAtOffset(0, -padding);
-                        stream.showText(k + ", proficiency: " + v + " / 10");
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-                });
+                try {
+                    stream.newLine();
+                    stream.newLineAtOffset(0, -padding);
+                    stream.showText(k + ", proficiency: " + v + " / 10");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
             stream.showText("• EDUCATION:");
             cv.getEducation().forEach((k, v) -> {
-                    try {
-                        stream.newLine();
-						stream.newLineAtOffset(0, -padding);
-                        stream.showText(k + ", register year: " + v);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-                });
+                try {
+                    stream.newLine();
+                    stream.newLineAtOffset(0, -padding);
+                    stream.showText(k + ", register year: " + v);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
             stream.showText("• PUBLICATIONS:");
+            stream.setRenderingMode(RenderingMode.FILL);
             cv.getPublications().forEach((k, v) -> {
-                    try {
-                        stream.newLine();
-						stream.newLineAtOffset(0, -padding);
-                        stream.showText(k + ", year: " + v);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-                });
+                try {
+                    stream.newLine();
+                    stream.newLineAtOffset(0, -padding);
+                    stream.showText(k + ", year: " + v);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         } else {
             Cv cv = c;
             System.out.println(width + " " + height);
             stream.newLineAtOffset(width / 8, height - padding);
-            stream.showText("• Full name: " + cv.getFullName());
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText(cv.getFullName() + "'s CV");
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Birth year: " + cv.getBirthYear());
+            stream.newLineAtOffset(0, -padding*2);
+            stream.showText("• Full name: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getFullName());
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• GPA: " + cv.getGpa());
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Birth year: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText("" + cv.getBirthYear());
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Email: " + cv.getHomeAddress());
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• GPA: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getGpa() + "");
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Description: " + cv.getHomeAddress());
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Email: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getHomeAddress());
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Home address: " + cv.getHomeAddress());
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Description: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getHomeAddress());
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Phone: " + cv.getPhone());
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Home address: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getHomeAddress());
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
-            stream.showText("• Website: " + cv.getWebsite());
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Phone: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getPhone());
             stream.newLine();
-            stream.newLineAtOffset(0, -padding);
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
+            stream.showText("• Website: ");
+            stream.setRenderingMode(RenderingMode.FILL);
+            stream.showText(cv.getWebsite());
+            stream.newLine();
+            stream.newLineAtOffset(0, -padding*2);
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
             stream.showText("• SKILLS:");
+            stream.setRenderingMode(RenderingMode.FILL);
             cv.getSkills().forEach((k, v) -> {
-                    try {
-                        stream.newLine();
-						stream.newLineAtOffset(0, -padding);
-                        stream.showText(k + ", proficiency: " + v + " / 10");
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-                });
-stream.newLineAtOffset(0, -padding);
+                try {
+                    stream.newLine();
+                    stream.newLineAtOffset(0, -padding*2);
+                    stream.showText(k);
+                    stream.showText(", proficiency: " + v);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            stream.newLineAtOffset(0, -padding*2);
             stream.newLine();
+            stream.setRenderingMode(RenderingMode.FILL_STROKE);
             stream.showText("• EDUCATION:");
+            stream.setRenderingMode(RenderingMode.FILL);
             cv.getEducation().forEach((k, v) -> {
-                    try {
-                        stream.newLine();
-						stream.newLineAtOffset(0, -padding);
-                        stream.showText(k + ", register year: " + v);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-                });
-            
-            
+                try {
+                    stream.newLine();
+                    stream.newLineAtOffset(0, -padding*2);
+                    stream.showText(k);
+                    stream.showText(", register year: " + v);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
         }
         stream.endText();
@@ -147,5 +209,6 @@ stream.newLineAtOffset(0, -padding);
         if (file != null) {
             document.save(file);
         }
+        document.close();
     }
 }
